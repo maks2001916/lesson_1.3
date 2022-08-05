f4f2496e2014d7a93d01932a1bb2cbe63ebb5baa
maks <mamonov.maksim.20003@gmail.com>
2022-08-05 08:57:42 +0300

    Первое задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        //первое задание
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+}
 
-        System.out.println("Hello world!");
+
     }
 }
