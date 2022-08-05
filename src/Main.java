369a22686af956033afc990a302f5c5c6eb0258d
maks <mamonov.maksim.20003@gmail.com>
2022-08-05 09:29:39 +0300

    Первое задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         //первое задание
-        int clientOS = 1;
+        byte clientOS = 1;
         if (clientOS == 1) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
+        //второе задание
+        int clientDeviceYear = 2018;
+        if (clientDeviceYear <= 2015 && clientOS == 1) {
+            System.out.println("установите облегчённую версию приложения для Android по ссылке");
+        }
+        if (clientDeviceYear <= 2015 && clientOS == 0) {
+            System.out.println("установите облегчённую версию приложения для IOS по ссылке");
+        }
+        if (clientDeviceYear > 2015 && clientOS == 1) {
+            System.out.println("установите мобильную версию приложения для Android по ссылке");
+        }
+        if (clientDeviceYear > 2015 && clientOS == 0) {
+            System.out.println("установите мобильную версию приложения для IOS по ссылке");
+        }
+
 }
 
 
